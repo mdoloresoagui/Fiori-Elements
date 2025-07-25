5c186ded1d7c2633c75851bd50bafaf5c93a462f
mdoloresoagui <mdoloresoagui@gmail.com>
2025-07-25 11:46:15 +0000

    Git v1

---
+++ webapp/controller/Main.controller.ts
@@ -0,0 +1,12 @@
+import Controller from "sap/ui/core/mvc/Controller";
+
+/**
+ * @namespace git.controller
+ */
+export default class Main extends Controller {
+
+    /*eslint-disable @typescript-eslint/no-empty-function*/
+    public onInit(): void {
+
+    }
+}
